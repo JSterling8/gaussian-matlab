e453fa9ad85de309277840936ad23597c37b3e5c
JSterling8 <JSterling8@gmail.com>
2016-02-05 15:51:59 +0000

    Added mandatory row swap test.  Added tic tocs to all test functions

--- stage3Tests.m
+++ stage3Tests.m
@@ -1,5 +1,6 @@
 %% Main function to generate tests
 function tests = stage3Tests
+    testMandatoryRowSwap();
     testTenByTenZeroValueMatrix();
     testOneByOneMatrix();
     testOneByOneZeroValueMatrix();
@@ -10,7 +11,9 @@ function tests = stage3Tests
 end
 
 function testOneByOneMatrix()
-        fprintf('Beginning 1x1 test.  Will test with 10 random 1x1 matrices\n')
+    fprintf('Beginning 1x1 test.  Will test with 10 random 1x1 matrices\n')
+    tic
+    
     for i = 1:10
         A = rand(1) .* 100;
         while rank(A) ~= 1
@@ -29,11 +32,13 @@ function testOneByOneMatrix()
         end
     end
     
+    toc
     fprintf('1x1 test passed\n\n')
 end
 
 function testOneByOneZeroValueMatrix()
     fprintf('Beginning 1x1 zero value test.\n')
+    tic
     
     caught = 0;
     try
@@ -46,11 +51,14 @@ function testOneByOneZeroValueMatrix()
     end
     
     assert(caught == 1, 'No exception thrown for unsolvable matrix');
-    fprintf('1x1 test passed\n\n')
+    
+    toc
+    fprintf('1x1 zero value test passed\n\n')
 end
 
 function testTenByTenZeroValueMatrix()
     fprintf('Beginning 10x10 zeroes test.\n')
+    tic
     
     caught = 0;
     try
@@ -63,11 +71,15 @@ function testTenByTenZeroValueMatrix()
     end
     
     assert(caught == 1, 'No exception thrown for unsolvable matrix');
+    
+    toc
     fprintf('10x10 zeroes test passed\n\n')
 end
 
 function testCorrectXValuesReturned100x100()
     fprintf('Beginning 100x100 test.  Will test with 10 random 100x100 matrices\n')
+    tic
+    
     for i = 1:10
         A = rand(100) .* 100;
         while rank(A) ~= 100
@@ -86,12 +98,36 @@ function testCorrectXValuesReturned100x100()
         end
     end
     
+    toc
     fprintf('100x100 test passed\n\n')
+end
+
+function testMandatoryRowSwap()
+    fprintf('Beginning mandatory row swap test\n')
+    tic
+    % The following won't have a solution unless a row swap occurs:
+    A = [0, 0, 1; 1, 2, 3; 4, 5, 6];
+    % Row one will have to be moved to the bottom.
+    b = [4; 3; 2];
+    x = A\b;
+    
+    x_calc = stage3(A, b);
+    
+    tolerance = 0.00000001;
+    for row = 1:3
+      if abs(x(row)) - abs (x_calc(row)) > tolerance
+          error('Calculated incorrect solution');
+      end
+    end
+    
+    toc
+    fprintf('Mandatory row swap test complete\n\n')
 end
 
 function testNxMFails()
     fprintf('Beginning NxM test (asserting error thrown).\n')
-
+    tic
+    
     caught = 0;
     try
         A = zeros(5,4);
@@ -101,12 +137,14 @@ function testNxMFails()
     end
     assert(caught == 1, 'No error was thrown for NxM matrix input');
     
+    toc
     fprintf('NxM test passed\n\n')
 end
 
 function testNonRealNumbersFail()
     fprintf('Beginning non-real number test (asserting error thrown).\n')
-
+    tic
+    
     caught = 0;
     try
         A = zeros(5,5);
@@ -117,12 +155,14 @@ function testNonRealNumbersFail()
     end
     assert(caught == 1, 'No error was thrown for non-real matrix input');
     
+    toc
     fprintf('Non-real number test test passed\n\n')
 end
 
 function testCorrectXValuesReturned1000x1000()
     fprintf('Beginning 1000x1000 test.  Will test with 1 random 1000x1000 matrices\n')
-
+    tic
+    
     A = rand(1000) .* 100;
     while rank(A) ~= 1000
         A = rand(1000) .* 100;
@@ -139,5 +179,6 @@ function testCorrectXValuesReturned1000x1000()
       end
     end
     
+    toc
     fprintf('1000x1000 test passed\n\n')
 end
